702df7b9fb09bd74c4520ccffcdfd56f218c14b7
Ján Kovačovský <kovacoj1@gmail.com>
2026-04-05 11:51:35 +0200

    problem(2833): easy counter

---
+++ problems/2833.furthest-point-from-origin.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=2833 lang=python3
+#
+# [2833] Furthest Point From Origin
+#
+
+# @lc code=start
+from collections import Counter
+
+class Solution:
+    def furthestDistanceFromOrigin(self, moves: str) -> int:
+        counter = Counter(moves)
+
+        return abs(counter['L'] - counter['R']) + counter['_']
+
+# @lc code=end
